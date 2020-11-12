d24b1d6cff21a1aad458605330a5341bfd0fc31e
Anders Langlands <anderslanglands@gmail.com>
2020-11-13 12:50:23 +1300

    switch namespace resolution method

--- test/half/example_output/half.cpp
+++ test/half/example_output/half.cpp
@@ -5,192 +5,192 @@
 namespace {
 #include "casts.h"
 
-CPPMM_DEFINE_POINTER_CASTS(Imath::half, Imath_half)
+CPPMM_DEFINE_POINTER_CASTS(Imath_3_0::half, Imath_3_0_half)
 
 #undef CPPMM_DEFINE_POINTER_CASTS
 }
 
 extern "C" {
-static_assert(sizeof(Imath::half) == sizeof(Imath_half), "sizes do not match");
-static_assert(alignof(Imath::half) == alignof(Imath_half), "alignments do not match");
+static_assert(sizeof(Imath_3_0::half) == sizeof(Imath_3_0_half), "sizes do not match");
+static_assert(alignof(Imath_3_0::half) == alignof(Imath_3_0_half), "alignments do not match");
 
-void Imath_half_setBits(Imath_half* self, unsigned short bits) {
+void Imath_3_0_half_setBits(Imath_3_0_half* self, unsigned short bits) {
     to_cpp(self)->setBits(bits);
 }
 
 
 
-Imath_half Imath_half_sNan() {
-    return bit_cast<Imath_half>(Imath::half::sNan());
+Imath_3_0_half Imath_3_0_half_sNan() {
+    return bit_cast<Imath_3_0_half>(Imath_3_0::half::sNan());
 }
 
 
 
-Imath_half Imath_half_qNan() {
-    return bit_cast<Imath_half>(Imath::half::qNan());
+Imath_3_0_half Imath_3_0_half_qNan() {
+    return bit_cast<Imath_3_0_half>(Imath_3_0::half::qNan());
 }
 
 
 
-Imath_half Imath_half_posInf() {
-    return bit_cast<Imath_half>(Imath::half::posInf());
+Imath_3_0_half Imath_3_0_half_posInf() {
+    return bit_cast<Imath_3_0_half>(Imath_3_0::half::posInf());
 }
 
 
 
-bool Imath_half_isDenormalized(const Imath_half* self) {
+bool Imath_3_0_half_isDenormalized(const Imath_3_0_half* self) {
     return to_cpp(self)->isDenormalized();
 }
 
 
 
-bool Imath_half_isNormalized(const Imath_half* self) {
+bool Imath_3_0_half_isNormalized(const Imath_3_0_half* self) {
     return to_cpp(self)->isNormalized();
 }
 
 
 
-void Imath_half_copy(Imath_half* self, const Imath_half* other) {
-    self = to_c(new (self) Imath::half(*to_cpp(other)));
+void Imath_3_0_half_copy(Imath_3_0_half* self, const Imath_3_0_half* other) {
+    self = to_c(new (self) Imath_3_0::half(*to_cpp(other)));
 }
 
 
 
-void Imath_half_dtor(Imath_half* self) {
+void Imath_3_0_half_dtor(Imath_3_0_half* self) {
     to_cpp(self)->~half();
 }
 
 
 
-Imath_half Imath_half_negInf() {
-    return bit_cast<Imath_half>(Imath::half::negInf());
+Imath_3_0_half Imath_3_0_half_negInf() {
+    return bit_cast<Imath_3_0_half>(Imath_3_0::half::negInf());
 }
 
 
 
-Imath_half Imath_half_neg(const Imath_half* self) {
-    return bit_cast<Imath_half>(-(*to_cpp(self)));
+Imath_3_0_half Imath_3_0_half_neg(const Imath_3_0_half* self) {
+    return bit_cast<Imath_3_0_half>(-(*to_cpp(self)));
 }
 
 
 
-Imath_half Imath_half_round(const Imath_half* self, unsigned int n) {
-    return bit_cast<Imath_half>(to_cpp(self)->round(n));
+Imath_3_0_half Imath_3_0_half_round(const Imath_3_0_half* self, unsigned int n) {
+    return bit_cast<Imath_3_0_half>(to_cpp(self)->round(n));
 }
 
 
 
-unsigned short Imath_half_bits(const Imath_half* self) {
+unsigned short Imath_3_0_half_bits(const Imath_3_0_half* self) {
     return to_cpp(self)->bits();
 }
 
 
 
-bool Imath_half_isNegative(const Imath_half* self) {
+bool Imath_3_0_half_isNegative(const Imath_3_0_half* self) {
     return to_cpp(self)->isNegative();
 }
 
 
 
-bool Imath_half_isInfinity(const Imath_half* self) {
+bool Imath_3_0_half_isInfinity(const Imath_3_0_half* self) {
     return to_cpp(self)->isInfinity();
 }
 
 
 
-Imath_half* Imath_half_assign(Imath_half* self, const Imath_half* other) {
+Imath_3_0_half* Imath_3_0_half_assign(Imath_3_0_half* self, const Imath_3_0_half* other) {
     *to_cpp(self) = *to_cpp(other);
     return self;
 }
 
 
 
-Imath_half* Imath_half_mul_assign_float(Imath_half* self, float f) {
+Imath_3_0_half* Imath_3_0_half_mul_assign_float(Imath_3_0_half* self, float f) {
     *to_cpp(self) *= f;
     return self;
 }
 
 
 
-Imath_half* Imath_half_add_assign(Imath_half* self, Imath_half h) {
-    *to_cpp(self) += bit_cast<Imath::half>(h);
+Imath_3_0_half* Imath_3_0_half_add_assign(Imath_3_0_half* self, Imath_3_0_half h) {
+    *to_cpp(self) += bit_cast<Imath_3_0::half>(h);
     return self;
 }
 
 
 
-bool Imath_half_isNan(const Imath_half* self) {
+bool Imath_3_0_half_isNan(const Imath_3_0_half* self) {
     return to_cpp(self)->isNan();
 }
 
 
 
-bool Imath_half_isZero(const Imath_half* self) {
+bool Imath_3_0_half_isZero(const Imath_3_0_half* self) {
     return to_cpp(self)->isZero();
 }
 
 
 
-float Imath_half_to_float(const Imath_half* self) {
+float Imath_3_0_half_to_float(const Imath_3_0_half* self) {
     return to_cpp(self)->operator float();
 }
 
 
 
-void Imath_half_ctor(Imath_half* self) {
-    self = to_c(new (self) Imath::half());
+void Imath_3_0_half_ctor(Imath_3_0_half* self) {
+    self = to_c(new (self) Imath_3_0::half());
 }
 
 
 
-Imath_half* Imath_half_add_assign_float(Imath_half* self, float f) {
+Imath_3_0_half* Imath_3_0_half_add_assign_float(Imath_3_0_half* self, float f) {
     *to_cpp(self) += f;
     return self;
 }
 
 
 
-Imath_half* Imath_half_div_assign(Imath_half* self, Imath_half h) {
-    *to_cpp(self) /= bit_cast<Imath::half>(h);
+Imath_3_0_half* Imath_3_0_half_div_assign(Imath_3_0_half* self, Imath_3_0_half h) {
+    *to_cpp(self) /= bit_cast<Imath_3_0::half>(h);
     return self;
 }
 
 
 
-bool Imath_half_isFinite(const Imath_half* self) {
+bool Imath_3_0_half_isFinite(const Imath_3_0_half* self) {
     return to_cpp(self)->isFinite();
 }
 
 
 
-Imath_half* Imath_half_sub_assign(Imath_half* self, Imath_half h) {
-    *to_cpp(self) -= bit_cast<Imath::half>(h);
+Imath_3_0_half* Imath_3_0_half_sub_assign(Imath_3_0_half* self, Imath_3_0_half h) {
+    *to_cpp(self) -= bit_cast<Imath_3_0::half>(h);
     return self;
 }
 
 
 
-void Imath_half_from_float(Imath_half* self, float f) {
-    self = to_c(new (self) Imath::half(f));
+void Imath_3_0_half_from_float(Imath_3_0_half* self, float f) {
+    self = to_c(new (self) Imath_3_0::half(f));
 }
 
 
 
-Imath_half* Imath_half_mul_assign(Imath_half* self, Imath_half h) {
-    *to_cpp(self) *= bit_cast<Imath::half>(h);
+Imath_3_0_half* Imath_3_0_half_mul_assign(Imath_3_0_half* self, Imath_3_0_half h) {
+    *to_cpp(self) *= bit_cast<Imath_3_0::half>(h);
     return self;
 }
 
 
 
-Imath_half* Imath_half_sub_assign_float(Imath_half* self, float f) {
+Imath_3_0_half* Imath_3_0_half_sub_assign_float(Imath_3_0_half* self, float f) {
     *to_cpp(self) -= f;
     return self;
 }
 
 
 
-Imath_half* Imath_half_div_assign_float(Imath_half* self, float f) {
+Imath_3_0_half* Imath_3_0_half_div_assign_float(Imath_3_0_half* self, float f) {
     *to_cpp(self) /= f;
     return self;
 }
--- test/half/example_output/half.h
+++ test/half/example_output/half.h
@@ -14,94 +14,94 @@ extern "C" {
 #define CPPMM_ALIGN(x) __attribute__((aligned(x)))
 #endif
 
-typedef struct { char _private[2]; } Imath_half CPPMM_ALIGN(2);
+typedef struct { char _private[2]; } Imath_3_0_half CPPMM_ALIGN(2);
 
 
-void Imath_half_setBits(Imath_half* self, unsigned short bits);
+void Imath_3_0_half_setBits(Imath_3_0_half* self, unsigned short bits);
 
 
-Imath_half Imath_half_sNan();
+Imath_3_0_half Imath_3_0_half_sNan();
 
 
-Imath_half Imath_half_qNan();
+Imath_3_0_half Imath_3_0_half_qNan();
 
 
-Imath_half Imath_half_posInf();
+Imath_3_0_half Imath_3_0_half_posInf();
 
 
-bool Imath_half_isDenormalized(const Imath_half* self);
+bool Imath_3_0_half_isDenormalized(const Imath_3_0_half* self);
 
 
-bool Imath_half_isNormalized(const Imath_half* self);
+bool Imath_3_0_half_isNormalized(const Imath_3_0_half* self);
 
 
-void Imath_half_copy(Imath_half* self, const Imath_half* other);
+void Imath_3_0_half_copy(Imath_3_0_half* self, const Imath_3_0_half* other);
 
 
-void Imath_half_dtor(Imath_half* self);
+void Imath_3_0_half_dtor(Imath_3_0_half* self);
 
 
-Imath_half Imath_half_negInf();
+Imath_3_0_half Imath_3_0_half_negInf();
 
 
-Imath_half Imath_half_neg(const Imath_half* self);
+Imath_3_0_half Imath_3_0_half_neg(const Imath_3_0_half* self);
 
 
-Imath_half Imath_half_round(const Imath_half* self, unsigned int n);
+Imath_3_0_half Imath_3_0_half_round(const Imath_3_0_half* self, unsigned int n);
 
 
-unsigned short Imath_half_bits(const Imath_half* self);
+unsigned short Imath_3_0_half_bits(const Imath_3_0_half* self);
 
 
-bool Imath_half_isNegative(const Imath_half* self);
+bool Imath_3_0_half_isNegative(const Imath_3_0_half* self);
 
 
-bool Imath_half_isInfinity(const Imath_half* self);
+bool Imath_3_0_half_isInfinity(const Imath_3_0_half* self);
 
 
-Imath_half* Imath_half_assign(Imath_half* self, const Imath_half* other);
+Imath_3_0_half* Imath_3_0_half_assign(Imath_3_0_half* self, const Imath_3_0_half* other);
 
 
-Imath_half* Imath_half_mul_assign_float(Imath_half* self, float f);
+Imath_3_0_half* Imath_3_0_half_mul_assign_float(Imath_3_0_half* self, float f);
 
 
-Imath_half* Imath_half_add_assign(Imath_half* self, Imath_half h);
+Imath_3_0_half* Imath_3_0_half_add_assign(Imath_3_0_half* self, Imath_3_0_half h);
 
 
-bool Imath_half_isNan(const Imath_half* self);
+bool Imath_3_0_half_isNan(const Imath_3_0_half* self);
 
 
-bool Imath_half_isZero(const Imath_half* self);
+bool Imath_3_0_half_isZero(const Imath_3_0_half* self);
 
 
-float Imath_half_to_float(const Imath_half* self);
+float Imath_3_0_half_to_float(const Imath_3_0_half* self);
 
 
-void Imath_half_ctor(Imath_half* self);
+void Imath_3_0_half_ctor(Imath_3_0_half* self);
 
 
-Imath_half* Imath_half_add_assign_float(Imath_half* self, float f);
+Imath_3_0_half* Imath_3_0_half_add_assign_float(Imath_3_0_half* self, float f);
 
 
-Imath_half* Imath_half_div_assign(Imath_half* self, Imath_half h);
+Imath_3_0_half* Imath_3_0_half_div_assign(Imath_3_0_half* self, Imath_3_0_half h);
 
 
-bool Imath_half_isFinite(const Imath_half* self);
+bool Imath_3_0_half_isFinite(const Imath_3_0_half* self);
 
 
-Imath_half* Imath_half_sub_assign(Imath_half* self, Imath_half h);
+Imath_3_0_half* Imath_3_0_half_sub_assign(Imath_3_0_half* self, Imath_3_0_half h);
 
 
-void Imath_half_from_float(Imath_half* self, float f);
+void Imath_3_0_half_from_float(Imath_3_0_half* self, float f);
 
 
-Imath_half* Imath_half_mul_assign(Imath_half* self, Imath_half h);
+Imath_3_0_half* Imath_3_0_half_mul_assign(Imath_3_0_half* self, Imath_3_0_half h);
 
 
-Imath_half* Imath_half_sub_assign_float(Imath_half* self, float f);
+Imath_3_0_half* Imath_3_0_half_sub_assign_float(Imath_3_0_half* self, float f);
 
 
-Imath_half* Imath_half_div_assign_float(Imath_half* self, float f);
+Imath_3_0_half* Imath_3_0_half_div_assign_float(Imath_3_0_half* self, float f);
 
 
 #undef CPPMM_ALIGN
