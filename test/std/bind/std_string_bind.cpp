4e2216f0577b629672c083a936ee4584254c62bc
Luke Titley <luke@titley.com>
2020-12-08 23:34:04 +0100

    Expose additional methods for testing std string

--- test/std/bind/std_string_bind.cpp
+++ test/std/bind/std_string_bind.cpp
@@ -12,7 +12,10 @@ namespace test_std_string {
 class Test
 {
 public:
-    std::string & get_value();
+    Test(const std::string & value);
+    void set_value(const std::string & value);
+    const std::string & get_value() const;
+    std::string & edit_value();
 };
 
 }
