e594d299b90394f66223be173d4eddb297fd4c1e
Luke Titley <luke@titley.com>
2020-12-09 22:08:07 +0100

    Remove the constructor, might help with finding build errors

--- test/std/bind/test_bind.cpp
+++ test/std/bind/test_bind.cpp
@@ -12,9 +12,10 @@ namespace test {
 class Test
 {
 public:
-    explicit Test(const std::string & value);
+    //explicit Test(const std::string & value);
     void set_value(const std::string & value);
     const std::string & get_value() const;
+    std::string clone_value() const;
     std::string & edit_value();
 };
 
