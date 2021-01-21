375bd20836e0bdceca466c93bdd4a55926e055c0
Luke Titley <luke@titley.com>
2021-01-21 22:35:55 +0100

    Added forward_declarations to CFile

--- src/c_file.hpp
+++ src/c_file.hpp
@@ -9,6 +9,7 @@ struct CFile {
     std::set<std::string> casts_macro_invocations;
     std::vector<std::string> pretty_defines;
     std::set<std::string> header_includes;
+    std::string forward_declarations;
     std::string declarations;
     std::string definitions;
 };
