a5a55454f1f21d92e7d80a8633d1a5d87f23ebda
Luke Titley <luke@titley.com>
2021-01-21 22:24:28 +0100

    Added a new struct called CFile. The aim is to group everything together that the c binding file will need

---
+++ src/cfile.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include<set>
+#include<string>
+#include<vector>
+
+/// Contains everything needed for the c body and header
+struct CFile {
+    std::set<std::string> casts_macro_invocations;
+    std::vector<std::string> pretty_defines;
+    std::set<std::string> includes;
+};
