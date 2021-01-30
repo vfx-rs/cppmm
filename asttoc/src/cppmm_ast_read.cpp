71e6a769c02a25db7d5adab2f47265a2c9b620ed
Luke Titley <luke@titley.com>
2021-01-31 00:01:48 +0100

    Started to bring online the translation unit reader

--- asttoc/src/cppmm_ast_read.cpp
+++ asttoc/src/cppmm_ast_read.cpp
@@ -5,7 +5,18 @@
 #include "cppmm_ast_read.hpp"
 #include "json.hh"
 
+namespace nln = nlohmann;
+
+//------------------------------------------------------------------------------
+NodeTranslationUnit read_translation_unit() {
+}
+
+//------------------------------------------------------------------------------
 Root read_json(std::istream & input) {
-    nlohmann::json j;
+    std::vector<NodePtr> nodes;
+
+    nln::json j;
     input >> j;
+
+    return Root(std::move(nodes));
 }
