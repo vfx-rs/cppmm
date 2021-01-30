fc83ad25eed3a59cdd7b3229776287d74ec8ea4a
Luke Titley <luke@titley.com>
2021-01-30 23:57:40 +0100

    Hook in nlohmann::json

--- asttoc/src/cppmm_ast_read.cpp
+++ asttoc/src/cppmm_ast_read.cpp
@@ -3,6 +3,9 @@
 // vfx-rs
 //------------------------------------------------------------------------------
 #include "cppmm_ast_read.hpp"
+#include "json.hh"
 
 Root read_json(std::istream & input) {
+    nlohmann::json j;
+    input >> j;
 }
