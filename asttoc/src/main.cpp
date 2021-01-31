ac5ef3f883364207206c6c6f5f7a4c1fd9b2fae1
Luke Titley <luke@titley.com>
2021-01-31 18:21:42 +0100

    Point the reader to the vec json output in test folder

--- asttoc/src/main.cpp
+++ asttoc/src/main.cpp
@@ -3,8 +3,16 @@
 //------------------------------------------------------------------------------
 //#include "cereal/cereal.hpp"
 #include "cppmm_ast.hpp"
+#include "cppmm_ast_read.hpp"
+
+#include <fstream>
 
 int main()
 {
+    std::ifstream input_file;
+    input_file.open("/Volumes/src/cppmm/test/imath/ref/imath_vec.json");
+
+    auto ast = cppmm::read_json(input_file);
+
     return 0;
 }
