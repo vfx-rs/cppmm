9f92f9f4cd5a0d548177f2b11a8d41d5984c18cd
Luke Titley <luke@titley.com>
2021-02-01 23:43:59 +0100

    Added stub entry points for the c transformation

---
+++ asttoc/include/cppmm_ast_to_c.hpp
@@ -0,0 +1,13 @@
+//------------------------------------------------------------------------------
+// vfx-rs
+//------------------------------------------------------------------------------
+#pragma once
+#include "cppmm_ast.hpp"
+
+namespace cppmm {
+namespace transform {
+
+Root add_c(const Root & root);
+
+} // namespace transform
+} // namespace cppmm
---
+++ asttoc/src/cppmm_ast_to_c.cpp
@@ -0,0 +1,14 @@
+//------------------------------------------------------------------------------
+// vfx-rs
+//------------------------------------------------------------------------------
+#include "cppmm_ast_to_c.hpp"
+
+namespace cppmm {
+namespace transform {
+
+Root add_c(const Root & root)
+{
+}
+
+} // namespace transform
+} // namespace cppmm
