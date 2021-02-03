d04bf899c90e3dbfe4b6542946cb6eeaf6aeb0cc
Luke Titley <luke@titley.com>
2021-02-03 01:07:05 +0100

    Started to push in place the code that will flatten methods

--- asttoc/src/cppmm_ast_add_c.cpp
+++ asttoc/src/cppmm_ast_add_c.cpp
@@ -58,8 +58,27 @@ void opaquebytes_record(NodeRecord & c_record)
 }
 
 //------------------------------------------------------------------------------
-void opaquebytes_methods(const NodeRecord & cpp_record, NodeRecord & c_record)
+void opaquebytes_method(TranslationUnit & c_tu, const NodeRecord & cpp_record,
+                        const NodeMethod & cpp_method)
 {
+    // Convert params
+    auto params = std::vector<Param>();
+    for(const auto & p : cpp_method.params)
+    {
+    }
+
+    // Convert return type
+    // Add opaquebytes body
+
+    // Add the new function to the translation unit
+}
+
+//------------------------------------------------------------------------------
+void opaquebytes_methods(TranslationUnit & c_tu, const NodeRecord & cpp_record)
+{
+    for(const auto & m: cpp_record.methods)
+    {
+    }
 }
 
 //------------------------------------------------------------------------------
@@ -80,9 +99,45 @@ void record(TranslationUnit & c_tu, const NodePtr & cpp_node)
     // So doing that first. Later will switch depending on the cppm attributes.
 
     opaquebytes_record(*c_record);
-    opaquebytes_methods(cpp_record, *c_record);
 
+    // Finally add the record to the translation unit
     c_tu.decls.push_back(std::move(c_record));
+}
+
+//------------------------------------------------------------------------------
+void methods(TranslationUnit & c_tu, const NodePtr & cpp_node)
+{
+    const auto & cpp_record =\
+        *static_cast<const NodeRecord*>(cpp_node.get());
+
+    // Most simple record implementation is the opaque bytes.
+    // Least safe and most restrictive in use, but easiest to implement.
+    // So doing that first. Later will switch depending on the cppm attributes.
+
+    opaquebytes_methods(c_tu, cpp_record);
+}
+
+//------------------------------------------------------------------------------
+TranslationUnit translation_unit(
+    const std::string & output_directory, const Root & root,
+    const size_t cpp_tu)
+{
+    // Create a new translation unit
+    const auto filepath =
+        generate::compute_c_filepath(output_directory,
+                                     root.tus[cpp_tu].filename);
+    auto c_tu = TranslationUnit(filepath);
+
+    // Loop over the record declarations
+    for (const auto & node : root.tus[cpp_tu].decls)
+    {
+        if (node->kind == NodeKind::Record)
+        {
+            generate::record(c_tu, node);
+        }
+    }
+
+    return c_tu;
 }
 
 } // namespace generate
@@ -93,27 +148,13 @@ void add_c(const std::string & output_directory, Root & root)
 {
     const auto tu_count = root.tus.size();
 
-    // Loop over all the translation units adding c equivalents
+    // Loop over all the translation units
     for (size_t i=0; i != tu_count; ++i)
     {
-        // For each cpp translation unit we add a c equivalent
-        const auto filepath =
-            generate::compute_c_filepath(output_directory,
-                                         root.tus[i].filename);
-        auto c_tu = TranslationUnit(filepath);
-
-        // Then we loop over the declarations converting the declarations
-        for (const auto & node : root.tus[i].decls)
-        {
-            // Handle the record
-            if (node->kind == NodeKind::Record)
-            {
-                generate::record(c_tu, node);
-            }
-        }
-
-        // Finally add the new translation unit
-        root.tus.push_back(std::move(c_tu));
+        // Add a new one for the c wrapper
+        root.tus.push_back(std::move(
+            generate::translation_unit(output_directory, root, i))
+        );
     }
 }
 
