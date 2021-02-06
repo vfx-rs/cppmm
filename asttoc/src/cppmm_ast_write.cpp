2319adcfdb9589725178902858ec30724023312f
Luke Titley <luke@titley.com>
2021-02-06 23:37:57 +0100

    Starting to rough out the code for the header and source generation

--- asttoc/src/cppmm_ast_write.cpp
+++ asttoc/src/cppmm_ast_write.cpp
@@ -3,14 +3,63 @@
 //------------------------------------------------------------------------------
 #include "cppmm_ast_write.hpp"
 
+#include "cppmm_ast.hpp"
+
+#include "pystring.h"
+
+#include <fmt/os.h>
+#include <cassert>
+
 namespace cppmm
 {
 class Root;
 
 namespace write
 {
+
+//------------------------------------------------------------------------------
+std::string compute_c_header_path(const std::string & path)
+{
+    std::string _;
+    std::string result;
+    pystring::os::path::splitext(result, _, path);
+    result += ".h";
+
+    return result;
+}
+
+//------------------------------------------------------------------------------
+void write_header(const TranslationUnit & tu)
+{
+    auto out = fmt::output_file(compute_c_header_path(tu.filename));
+    //out.print("Don't {}", "Panic");
+}
+
+//------------------------------------------------------------------------------
+void write_source(const TranslationUnit & tu)
+{
+    auto out = fmt::output_file(tu.filename);
+    //out.print("Don't {}", "Panic");
+}
+
+//------------------------------------------------------------------------------
+void write_translation_unit(const TranslationUnit & tu)
+{
+    write_header(tu);
+    write_source(tu);
+}
+
+//------------------------------------------------------------------------------
 void cpp(const Root & root, size_t starting_point)
 {
+    assert(starting_point < root.tu.size());
+
+    const auto size = root.tus.size();
+    for(size_t i=starting_point; i < size; ++i)
+    {
+        const auto & tu = root.tus[i];
+        write_translation_unit(tu);
+    }
 }
 } // namespace read
 } // namesapce cppmm
