f500c0a7a0d728e74ba23dd940513be4f6a71861
Anders Langlands <anderslanglands@gmail.com>
2021-02-17 20:03:40 +1300

    rename binary and hacky diff path ignore

--- test/runtest.py
+++ test/runtest.py
@@ -3,31 +3,32 @@ import sys
 import subprocess
 import shutil
 
-cppmm_exe = sys.argv[1]
+astgen_exe = sys.argv[1]
 binding_dir = sys.argv[2]
 output_dir = sys.argv[3]
 ref_dir = sys.argv[4]
 if len(sys.argv) >= 5:
-    cppmm_args = sys.argv[5:]
+    astgen_args = sys.argv[5:]
 else:
-    cppmm_args = []
+    astgen_args = []
 
 # Clean up an existing output directory and make sure it exists fresh
 shutil.rmtree(output_dir, ignore_errors=True)
 os.makedirs(output_dir)
 
-args = [cppmm_exe, binding_dir, '-o', output_dir, '--'] + cppmm_args
+args = [astgen_exe, binding_dir, '-o', output_dir, '--'] + astgen_args
 print('Running ' + ' '.join(args))
 
 result = subprocess.Popen(args, stderr=subprocess.STDOUT, stdout=subprocess.PIPE) 
 (stdout, _) = result.communicate(None)
 
 if result.returncode != 0:
-    print('cppmm exited with non-zero return code {}'.format(result.returncode))
+    print('astgen exited with non-zero return code {}'.format(result.returncode))
     print(stdout)
     sys.exit(result.returncode)
 
-result = subprocess.Popen(['diff', output_dir, ref_dir], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+# diff with a crummy attempt to ignore paths
+result = subprocess.Popen(['diff', '-I.*\"\/.*\/.*', output_dir, ref_dir], stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
 (stdout, _) = result.communicate(None)
 
 if result.returncode != 0:
