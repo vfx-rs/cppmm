897b91ae244ffe20d4a38e2807cf1f064ac37938
Luke Titley <luke@titley.com>
2021-03-01 23:47:10 +0100

    Re-enable astgen

--- bind.sh
+++ bind.sh
@@ -3,18 +3,18 @@ project=openexr
 pushd build
 
 # Generate the ast
-#./astgen/astgen ../test/$project/bind -u              \
-#    -o ../test/$project/ref/ast                   \
-#    -i $PWD/../test/$project/bind             \
-#    -rust-sys $PWD/../test/std/rust      \
-#    --                                   \
-#    -I/Volumes/src/cppmm/test/                   \
-#    -I/Volumes/src/cppmm/test/$project/bind                   \
-#    -I/Volumes/src/packages/usr/local/include \
-#    -I/Volumes/src/packages/usr/local/include/OpenEXR \
-#    -isysroot/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk \
-#    -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/include/c++/v1 \
-#    -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/lib/clang/11.0.0/include
+./astgen/astgen ../test/$project/bind -u              \
+    -o ../test/$project/ref/ast                   \
+    -i $PWD/../test/$project/bind             \
+    -rust-sys $PWD/../test/std/rust      \
+    --                                   \
+    -I/Volumes/src/cppmm/test/                   \
+    -I/Volumes/src/cppmm/test/$project/bind                   \
+    -I/Volumes/src/packages/usr/local/include \
+    -I/Volumes/src/packages/usr/local/include/OpenEXR \
+    -isysroot/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk \
+    -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/include/c++/v1 \
+    -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/lib/clang/11.0.0/include
 
 # Generate the cpp
 rm -rf out
