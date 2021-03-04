54cc3156fd07e12210fdc1af30d99adb2f66c313
Luke Titley <luke@titley.com>
2021-03-04 23:32:10 +0100

    Update bind.sh util script with the right parameters for generating openexr project

--- bind.sh
+++ bind.sh
@@ -3,22 +3,25 @@ project=openexr
 pushd build
 
 # Generate the ast
-./astgen/astgen ../test/$project/bind -u              \
-    -o ../test/$project/ref/ast                   \
-    -i $PWD/../test/$project/bind             \
-    -rust-sys $PWD/../test/std/rust      \
-    --                                   \
-    -I/Volumes/src/cppmm/test/                   \
-    -I/Volumes/src/cppmm/test/$project/bind                   \
-    -I/Volumes/src/packages/usr/local/include \
-    -I/Volumes/src/packages/usr/local/include/OpenEXR \
-    -isysroot/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk \
-    -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/include/c++/v1 \
-    -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/lib/clang/11.0.0/include
+#./astgen/astgen ../test/$project/bind -u              \
+#    -o ../test/$project/ref/ast                   \
+#    -rust-sys $PWD/../test/std/rust      \
+#    --                                   \
+#    -I/Volumes/src/cppmm/test/                   \
+#    -I/Volumes/src/packages/include \
+#    -I/Volumes/src/packages/include/OpenEXR \
+#    -isysroot/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk \
+#    -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/include/c++/v1 \
+#    -isystem /Volumes/src/clang+llvm-11.0.0-x86_64-apple-darwin/lib/clang/11.0.0/include
+##
+##-i $PWD/../test/$project/bind             \
+#-I/Volumes/src/cppmm/test/$project/bind                   \
+
+#cp ../test/openexr/imf_attribute_patched.json ../test/openexr/ref/ast/imf_attribute.json
 
 # Generate the cpp
 rm -rf out
 mkdir -p out
-./asttoc/asttoc -L /Volumes/src/packages/usr/local/lib -l Imath -o out ../test/$project/ref/ast
+./asttoc/asttoc -L /Volumes/src/packages/lib -l Imath -l IlmImf -l IlmImfUtil -l Half -l IlmThread -l Iex -l IexMath -o out ../test/$project/ref/ast
 
 popd
