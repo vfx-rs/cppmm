1af09d1c6a277dd65732407f239e26441b40f1a8
Anders Langlands <anderslanglands@gmail.com>
2021-04-07 20:09:41 +1200

    simple std vec/string test

---
+++ test/std/bind/c-usestd.cpp
@@ -0,0 +1,12 @@
+#include <usestd.hpp>
+
+namespace cppmm_bind {
+
+namespace usestd {
+
+void takes_string(const ::std::string& s);
+void takes_vector_string(const ::std::vector<::std::string>& v);
+
+} // namespace usestd
+
+} // namespace cppmm_bind
