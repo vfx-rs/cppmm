b4282c95f5b4a38f09aa56901de5e5e2062e3b37
Anders Langlands <anderslanglands@gmail.com>
2021-04-12 21:59:51 +1200

    add string back in

--- test/std/bind/std_string.cpp
+++ test/std/bind/std_string.cpp
@@ -7,24 +7,24 @@ namespace cppmm_bind {
 
 namespace std {
 
-// class basic_string {
-// public:
-//     using BoundType = ::std::string;
+class basic_string {
+public:
+    using BoundType = ::std::string;
 
-//     // We can't match against the constructor without specifying an allocator
-//     // parameter, but we definitely don't want to be passing that from C so
-//     // we could use CPPMM_IGNORE here to indicate that we allow this
-//     parameter
-//     // to be defaulted in the bindings. We'll want to check that there is in
-//     // fact a default supplied in the library of course...
-//     basic_string(const char* s, ::std::string::size_type count,
-//                  const ::std::string::allocator_type& alloc CPPMM_IGNORE);
-//     ::std::string& assign(const char* s, ::std::string::size_type count);
-//     const char* c_str() const;
+    // We can't match against the constructor without specifying an allocator
+    // parameter, but we definitely don't want to be passing that from C so
+    // we could use CPPMM_IGNORE here to indicate that we allow this
+    // parameter
+    // to be defaulted in the bindings. We'll want to check that there is in
+    // fact a default supplied in the library of course...
+    basic_string(const char* s, ::std::string::size_type count,
+                 const ::std::string::allocator_type& alloc CPPMM_IGNORE);
+    ::std::string& assign(const char* s, ::std::string::size_type count);
+    const char* c_str() const;
 
-// } CPPMM_OPAQUEBYTES;
+} CPPMM_OPAQUEBYTES;
 
-// using string = ::std::string;
+using string = ::std::string;
 
 template <class T> class vector {
 public:
