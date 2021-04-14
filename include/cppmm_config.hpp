c76fb85c8728a54f5dd1b31d66776d0326dea813
Anders Langlands <anderslanglands@gmail.com>
2021-04-15 11:18:41 +1200

    Add installation

---
+++ include/cppmm_config.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#define CPPMM_MAJOR_VERSION 0
+#define CPPMM_MINOR_VERSION 1
+#define CPPMM_PATCH_VERSION 0
+
+namespace cppmm {
+
+inline const char* version() {
+    return "0.1.0";
+}
+
+}
---
+++ package.py
@@ -0,0 +1,5 @@
+name = 'cppmm'
+version = '0.1.0'
+
+def commands():
+    env.PATH.append('{root}/bin')
