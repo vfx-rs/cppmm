3c25290d6a78e48cf3ef8d9540d3b73d5278ecd1
Luke Titley <luke@titley.com>
2021-05-26 22:59:56 +0200

    Added a new attribute 'CPPMM_FORCE_NAME' which explicitly ignores renames and overrides the qualified name returned by clang

--- astgen/src/astgen.cpp
+++ astgen/src/astgen.cpp
@@ -206,6 +206,7 @@ int main(int argc_, const char** argv_) {
 
 #define CPPMM_THROWS(EX, VAR) __attribute__((annotate("cppmm|throws|" #EX "|" #VAR)))
 #define CPPMM_NOEXCEPT __attribute__((annotate("cppmm|noexcept")))
+#define CPPMM_FORCE_NAME(x) __attribute__((annotate("cppmm|force_name|" #x)))
 
 #define CPPMM_ENUM_PREFIX(x) __attribute__((annotate("cppmm|enum_prefix|" #x)))
 #define CPPMM_ENUM_SUFFIX(x) __attribute__((annotate("cppmm|enum_suffix|" #x)))
